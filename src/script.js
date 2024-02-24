22a125a7db56e1bd99b555fecb7782b2d981ae94
Edmar <edmar.contato149@gmail.com>
2024-02-24 18:26:52 -0300

    initial commit

---
+++ src/script.js
@@ -0,0 +1,12 @@
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function calculate() {
+    try {
+        let result = eval(document.getElementById('display').value);
+        document.getElementById('display').value = result;
+    } catch (error) {
+        document.getElementById('display').value = 'Error'
+    }
+}
